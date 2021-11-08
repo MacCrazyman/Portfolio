b022cffde21bbaa5e823f451aa9bcfb010ab52d6
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-11-08 15:56:10 -0500

    Add close function to menu items

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('#mobile_menu');
-const cross = document.querySelector('#close_menu');
+const cross = document.querySelectorAll('.close_menu');
 
 function displayMenu() {
   mobileMenu.classList.replace('hidden', 'mobile_menu');
@@ -11,4 +11,4 @@ function hideMenu() {
 }
 
 hamburger.addEventListener('click', displayMenu);
-cross.addEventListener('click', hideMenu);
+cross.forEach((item) => item.addEventListener('click', hideMenu));
