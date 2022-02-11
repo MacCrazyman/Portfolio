9846e41b2955d95068ea6d193df31ee3e5bb008a
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-11 13:27:38 -0500

    Fetch and update zen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 // import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/home';
+import { fetchZen } from './redux/features/github/githubSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchZen());
+  });
   return (
     <div className="App">
       <Header />
--- src/redux/features/github/githubSlice.js
+++ src/redux/features/github/githubSlice.js
@@ -1,6 +1,20 @@
 /* eslint-disable no-param-reassign -- state protected from mutating by using immer */
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+export const fetchZen = createAsyncThunk(
+  'github/fetchZen',
+  async () => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+    const response = await fetch('https://api.github.com/zen', requestOptions)
+      .then((response) => response.text())
+      .catch((error) => console.log('error', error));
+
+    return response;
+  },
+);
 export const githubSlice = createSlice({
   name: 'github',
   initialState: {
@@ -12,6 +26,12 @@ export const githubSlice = createSlice({
     addZen: (state, action) => {
       state.zen = action.payload;
     },
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchZen.fulfilled, (state, action) => {
+      state.zen = action.payload;
+    });
   },
 
 });
