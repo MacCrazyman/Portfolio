a92f58a19e46e9c89722fa3ac1081829f9acec7e
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-13 12:17:38 -0500

    Add header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,40 @@
+import {
+  Badge,
+  Container, Nav, Navbar, NavDropdown,
+} from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
   const github = useSelector((state) => state.github);
   return (
-    <header>
-      {github.zen}
-    </header>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          Leonardo Pareja
+          <Badge>
+            {' '}
+            {github.zen}
+            {' '}
+          </Badge>
+          {' '}
+
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
